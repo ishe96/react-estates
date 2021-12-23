60acf52da061fcbd8cde510c264bd895b047048e
Isheanesu Madamombe <amadamombe904@gmail.com>
2021-12-23 21:02:51 +0200

    Final Touchies

--- pages/search.js
+++ pages/search.js
@@ -3,9 +3,13 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { BsFilter } from "react-icons/bs";
-import SearchFilters from "../components/SearchFilters";
 
-const Search = () => {
+import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import noresult from "../assets/images/noresult.svg";
+import { fetchApi, baseUrl } from "../utils/fetchApi";
+
+const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -25,9 +29,59 @@ const Search = () => {
                 <Text>Search</Text>
                 <Icon paddingLeft="2" w="7" as={BsFilter} />
             </Flex>
-            {searchFilters && <SearchFilters/>}
+            {searchFilters && <SearchFilters />}
+
+            <Text fontSize="2xl" p="4" fontWeight="bold">
+                Properties {router.query.purpose}
+            </Text>
+
+            <Flex flexWrap="wrap">
+                {properties.map((property) => (
+                    <Property property={property} key={property.id} />
+                ))}
+            </Flex>
+            {properties.length === 0 && (
+                <Flex
+                    justifyContent="center"
+                    alignItems="centre"
+                    marginTop="5"
+                    marginBottom="5"
+                    flexDirection="column"
+                >
+                    <Image alt="no result" src={noresult} />
+                    <Text fontSize="2xl" marginTop="3" textAlign="center">
+                        {" "}
+                        No Results Found
+                    </Text>
+                </Flex>
+            )}
         </Box>
     );
 };
+
+
+
+// ServerSidePropsSearch.js
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || 'for-rent';
+  const rentFrequency = query.rentFrequency || 'yearly';
+  const minPrice = query.minPrice || '0';
+  const maxPrice = query.maxPrice || '1000000';
+  const roomsMin = query.roomsMin || '0';
+  const bathsMin = query.bathsMin || '0';
+  const sort = query.sort || 'price-desc';
+  const areaMax = query.areaMax || '35000';
+  const locationExternalIDs = query.locationExternalIDs || '5002';
+  const categoryExternalID = query.categoryExternalID || '4';
+
+  const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
+
 
 export default Search;
